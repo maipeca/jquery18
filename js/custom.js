82a67c2b124f63e9fea981ff312294eb689180d4
Maite <maipeca@hotmail.com>
2022-09-11 16:00:53 +0200

    jquery ui plugins

--- js/custom.js
+++ js/custom.js
@@ -44,15 +44,13 @@ $(document).ready(function(){
     // $('.featured-item:first h4').fadeOut()
     //  $('.featured-item:first h4').css('color', '#f00')
      
-     $('.featured-item h4').dblclick( function(){
+   $('.featured-item h4').dblclick( function(){
 
         $(this).css({
             'color': '#f00',
             'background': '#050533',
             'font-weight': '100',
         });
-
-       
      });
 
      /*
@@ -67,13 +65,153 @@ $(document).ready(function(){
      })
      
      
-     
-     
+     $('#form-submit').on('click', function(e){
+
+      e.preventDefault()
+
+      if( $('#email').val() != '' ){
+
+         $('#email').animate({
+            opacity: "toggle",
+           
+         }, 500, function(){
+            console.log($(this).val())
+         })
+
+      }
 
 
+   });
+
+ 
+
+   /*
+    * Animações
+    */
+   const duracao = 1000 // equivalenta a 1 segundo
+
+   $('.featured-item:nth(0)').hide(duracao).show(duracao)
+   $('.featured-item:nth(1)').hide(duracao).show(duracao)
+   $('.featured-item:nth(2)').hide(duracao).show(duracao)
+ 
+
+   $('#form-submit').on('click', function(e){
+
+      e.preventDefault()
+
+      if( $('#email').val() != '' ){
+
+         $('#email').animate({
+            opacity: "toggle",
+           
+         }, 500, function(){
+            console.log($(this).val())
+         })
+
+      }
 
 
+   });
 
 
+   /*
+    * Ouvinte de eventos .nav-modal-open
+    */
+   $('.nav-modal-open').on('click', function(e){
+
+      e.preventDefault();
+
+      let elem = $(this).attr('rel')
+
+      $('.modal-body').html($('#'+elem).html())
+      
+      $('.modal-header h5.modal-title').html($(this).text())
+
+      let myModal = new bootstrap.Modal($('#modelId'))
+
+      myModal.show()
+
+
+   })
+
+
+   /*
+    * TODO: incrementar a validação
+    * - checar se o nome é válido (mais de 2 caracteres)
+    * - checar se o email é válido com ao menos um "@" e "."
+    * - checar se o cpf é válido com regex
+    */
+   function validate( elem ){
+      if( elem.val() == '') {
+
+         console.log('o campo de '+ elem.attr('name') + ' é obrigatório')
+
+         elem.parent().find('.text-muted').show()
+
+         elem.addClass('invalid')
+
+         return false
+      } else {
+         elem.parent().find('.text-muted').hide()
+         elem.removeClass('invalid')
+      }
+   }
+
+   $('body').on('submit', '.modal-body .form', function(e){
+
+      e.preventDefault()
+
+      const inputName = $('#nome')
+      const inputEmail = $('#email')
+
+      validate(inputName)
+      validate(inputEmail)
+
+      if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid')){
+         console.log('verificar campos obrigatórios')
+         return false
+      } else {
+         $(this).submit()  
+      }
+
+   })
+
+   $('body').on('blur', '#nome', function(){
+      validate($(this))
+   })
+
+   $('body').on('blur', '#email', function(){
+      validate($(this))
+   })
+
+
+   $('body').on('focus', '#date', function(){
+      $(this).datepicker()
+   })
+
+   $('body').on('blur', '#date', function(){
+      validate($(this))
+      $(this).mask('00/00/0000');
+   }) 
+
+   $('body').on('blur', '#time', function(){
+      validate($(this))
+      $(this).mask('00:00');
+   })
+
+   $('body').on('blur', '#cep', function(){
+      validate($(this))
+      $(this).mask('00000-000');
+   })
+
+   $('body').on('blur', '#phone', function(){
+      validate($(this))
+      $(this).mask('00000-0000');
+   })
+
+   $('body').on('blur', '#cpf', function(){
+      validate($(this))
+      $(this).mask('000.000.000-00');
+   })
 
 })
